6152a7f71c3f8c90ea4e232821961e7206ed4027
Oliver Bryant <oliver@ufomail.co.uk>
2015-06-27 21:08:12 +0000

    Evolved Plutus to use directories to download to.

---
+++ PlutusEvo.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+import csv
+import os
+
+'''
+Plutus - a micro CSV downloader written in Python
+
+-- Updated with multiple download endpoints --
+'''
+
+
+def main():
+    subroute()
+
+def first(): # endpoint one
+    url = ("http://example")
+    b = requests.get(url)
+    name_of_file = 'save'
+    save_path = 'data' # location
+    complete = os.path.join(save_path, name_of_file+'.csv')
+    with open(complete, "wb") as output:
+        output.write(b.content)
+    output.close()
+
+def second(): # endpoint two
+    url = ("http://example") #url
+    c = requests.get(url) #requests get module
+    name_of_file = 'savetwo'
+    save_path = 'data'
+    complete = os.path.join(save_path, name_of_file+'.csv')
+    with open("savetwo.csv", "wb") as output: # save file
+        output.write(c.content)
+    output.close() // close file
+
+def subroute(): // subroute (main dispatch and control)
+    first()
+    second()
+    sys.exit() // terminate after use.
+
+
+while __name__ == "__main__":
+    main()
